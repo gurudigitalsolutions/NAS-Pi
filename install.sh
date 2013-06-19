cff7912dc6be6a95d09533a4bbb6b9ed2c83868d
Chad Gould <gouldcwilliam@gmail.com>
2013-06-19 14:26:44 -0400

    backend/etc/init.d/naspid -> now sources /etc/naspi/naspid.conf backend/etc/naspi/naspid.d/start.inc -> commented the debug set -x backend/etc/naspi/naspid.d/stop.inc -> created a cleaner way to stop all processes backend/usr/bin/naspid -> commented the debug set -x install.sh -> fixed the username issues, naspid is now naspi

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@
 
 DEPENDENCIES=( samba smbclient apache2 php5 php5-cli php5-curl apache2-mpm-itk sshfs git curlftpfs netcat-openbsd)
 
-USER="naspid"
+USER="naspi"
 #~ WWWUSER="naspi"
 
 WWW="/usr/share/naspi"
@@ -185,12 +185,12 @@ function create_empty_directories
 		
 		if [[ ! -e "$WWW"/${empty} ]];then
 			mkdir -p -m 775 "$WWW"/${empty}
-			chown "naspi":"naspi" "$WWW"/${empty}
+			chown "$USER:$USER" "$WWW"/${empty}
 		fi
 
 	done
 	touch $WWW/log/error.log
-	chown "naspi:naspi" "$WWW/log/error.log"
+	chown "$USER:$USER" "$WWW/log/error.log"
 
 }
 
@@ -222,6 +222,6 @@ place_files
 create_empty_directories
 place_backend_files
 service apache2 restart
-service naspid start
+service naspid restart
 cd $START_DIR
 echo "NAS-Pi has been installed."
