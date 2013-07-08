731b475aea30b727a37574e9e2bcc435dcb5be07
Brian Murphy <gurudvlp@gmail.com>
2013-07-08 17:58:59 -0400

    Added an example bash script that handles backend operations from the daemon.

---
+++ backend/usr/share/naspi/pd/modules/bashexample/bashexample.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+#####################################################################################
+#
+#	NAS-Pi Backend Bash Script Example
+#
+#	This script is an example of a bash script that can be used by the NAS-Pi daemon
+#	to perform back-end operations.
+#
+#####################################################################################
+
+echo "Bash-Example Backend echoin' out"
