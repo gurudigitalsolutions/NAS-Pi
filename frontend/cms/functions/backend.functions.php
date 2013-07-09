0d4c8e3b051c243155524bfafa41369fe3e80d6d
Brian Murphy <gurudvlp@gmail.com>
2013-07-08 20:55:30 -0400

    Attempted to fix a possible infinite loop in the DaemonRawCommand CMS function.

--- frontend/cms/functions/backend.functions.php
+++ frontend/cms/functions/backend.functions.php
@@ -14,6 +14,8 @@ function DaemonModuleCommand($modcode, $command)
 function DaemonRawCommand($command)
 {
 	$socket = socket_create(AF_UNIX, SOCK_STREAM, 0);
+	
+	if($socket === false) { return "FAIL Could not create socket."; }
 	socket_connect($socket, "/tmp/naspi/pd/pd.sock");
 	
 	socket_write($socket, $command, strlen($command));
@@ -23,8 +25,8 @@ function DaemonRawCommand($command)
 	while($KeepReading)
 	{
 		$buffer = socket_read($socket, 2048, PHP_NORMAL_READ);
-		if(strlen($buffer) == 0) { break; }
-		else if($buffer === false) { break; }
+		if(strlen($buffer) == 0) { $KeepReading = false; break; }
+		else if($buffer === false) { $KeepReading = false; break; }
 		
 		$toret = $toret.$buffer;
 	}
